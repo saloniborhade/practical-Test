7000db94eff29f44463fc5dd9183a308a591ac97
Abc <Abc@DESKTOP-CSGNKBC>
2022-12-02 14:15:34 +0530

    Code

---
+++ 01-Dec-2022-PracticalTest/src/com/mygame/dao/GameDAO.java
@@ -0,0 +1,11 @@
+package com.mygame.dao;
+
+public class GameDAO {
+	static String CityName[]= {"Chennai","Mumbai","Dehli","Kolkata","Hydrabad"};
+	
+	public String[]getCityName(){
+		return CityName;
+	}
+
+
+}
---
+++ 01-Dec-2022-PracticalTest/src/com/mygame/service/GameService.java
@@ -0,0 +1,14 @@
+package com.mygame.service;
+import com.mygame.dao.GameDAO;
+
+public class GameService {
+	
+	public String[] getCityNameService() {
+		GameDAO GdObj=new GameDAO();
+		 GdObj.getCityName();
+		 String s[]=GdObj. getCityName();
+		 return s;
+	 
+	}
+
+}
